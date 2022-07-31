541883ad52dda187a296c744a85d198ac72a4251
wisnu kurniawan <wisnuko13@gmail.com>
2022-07-31 13:42:51 +0700

    Initial Commit

---
+++ wallee/walleeApp.swift
@@ -0,0 +1,17 @@
+//
+//  walleeApp.swift
+//  wallee
+//
+//  Created by Wisnu Kurniawan on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct walleeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
