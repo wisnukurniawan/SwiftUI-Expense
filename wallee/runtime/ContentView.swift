72fbb5c882c0db607258c972b35a4da9b69eb954
wisnu kurniawan <wisnuko13@gmail.com>
2022-08-03 14:47:08 +0700

    Add crashlytics script

--- wallee/runtime/ContentView.swift
+++ wallee/runtime/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      
+        
+        Button("Crash") {
+          fatalError("Crash was triggered")
+        }
     }
 }
 
